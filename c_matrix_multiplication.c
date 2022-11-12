be748b152cfb6e6ceecfdc457d48755f0fdda87a
Bobby Jack <bobbyjack@gmail.com>
2022-11-12 22:45:06 +0000

    refactor

--- c_matrix_multiplication.c
+++ c_matrix_multiplication.c
@@ -1,4 +1,6 @@
 #include<stdio.h>    
+#include<stdlib.h>
+
 int main()
 {
     int r1,r2,c1,c2;
@@ -13,47 +15,46 @@ int main()
     if(c1!=r2)
     {
         printf("The matrices cannot be multiplied together");
+        exit(-1);
     }
-    else
-    {
-        int m1[r1][c1],m2[r2][c2];
-        printf("Enter the elements of the first matrix\n");    
-        for(int i=0;i<r1;i++)    
+
+    int m1[r1][c1],m2[r2][c2];
+    printf("Enter the elements of the first matrix\n");    
+    for(int i=0;i<r1;i++)    
+    {    
+        for(int j=0;j<c1;j++)    
         {    
-            for(int j=0;j<c1;j++)    
-            {    
-                scanf("%d",&m1[i][j]);    
-            }    
+            scanf("%d",&m1[i][j]);    
         }    
-        printf("Enter elements of the second matrix\n");    
-        for(int i=0;i<r2;i++)    
+    }    
+    printf("Enter elements of the second matrix\n");    
+    for(int i=0;i<r2;i++)    
+    {    
+        for(int j=0;j<c2;j++)    
         {    
-            for(int j=0;j<c2;j++)    
-            {    
-                scanf("%d",&m2[i][j]);    
-            }    
+            scanf("%d",&m2[i][j]);    
         }    
-        int mul[r1][c2];
-        for(int i=0;i<r1;i++)    
+    }    
+    int mul[r1][c2];
+    for(int i=0;i<r1;i++)    
+    {    
+        for(int j=0;j<c2;j++)    
         {    
-            for(int j=0;j<c2;j++)    
+            mul[i][j]=0; 
+            for(int k=0;k<c1;k++)    
             {    
-                mul[i][j]=0; 
-                for(int k=0;k<c1;k++)    
-                {    
-                    mul[i][j]+=m1[i][k]*m2[k][j];    
-                } 
-            }    
+                mul[i][j]+=m1[i][k]*m2[k][j];    
+            } 
         }    
-        printf("The multiplied matrix is:\n");     
-        for(int i=0;i<r1;i++)    
+    }    
+    printf("The multiplied matrix is:\n");     
+    for(int i=0;i<r1;i++)    
+    {    
+        for(int j=0;j<c2;j++)    
         {    
-            for(int j=0;j<c2;j++)    
-            {    
-                printf("%d\t",mul[i][j]);    
-            }    
-            printf("\n");    
-        } 
-    }
+            printf("%d\t",mul[i][j]);    
+        }    
+        printf("\n");    
+    } 
     return 0;  
 }
